28a13e44668fd0fd81d8810ec781d34ca5c915fe
davidukim <39535370+davidukim@users.noreply.github.com>
2020-07-31 14:15:50 -0400

    Create iterator.js

---
+++ iterator.js
@@ -0,0 +1,28 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> myList = new LinkedList<>();
+        myList.add("item 1");
+        myList.add("item 2");
+        myList.add("item 3");
+        myList.add("item 4");
+        myList.add("item 5");
+        myList.add("item 6");
+        myList.add("item 7");
+        myList.add("item 8");
+
+        printList(myList);
+    }
+
+    public static void printList(List<String> list){
+        Iterator<String> iter = list.iterator();
+
+        while(iter.hasNext()){
+            System.out.println(iter.next());
+        }
+        System.out.println("==================");
+    }
+}
